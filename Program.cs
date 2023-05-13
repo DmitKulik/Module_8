96acb499c4e13bee811b46f2857deb9ecd2e3deb
Dmitry <grammattika@gmail.com>
2023-05-14 01:06:34 +0300

    создаем файл и перемещаем в корзину

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IO;
 
-namespace Module_8{
-    internal class Program{
+namespace Module_8
+{
+    internal class Program
+    {
         static void Main()
         { // Получить все файлы и папки корневого каталога: 
             {
@@ -11,53 +13,17 @@ namespace Module_8{
 
             static void GetCatalogs()
             {
-                string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
-                if (Directory.Exists(dirName)) // Проверим, что директория существует
-                {
-                    Console.WriteLine("Папки:");
-                    string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
 
-                    foreach (string d in dirs) // Выведем их все
-                        Console.WriteLine(d);
+                DirectoryInfo newDirectory = new DirectoryInfo(@"C:/1/newDirectory");// создание новой директории в корне вашего диска
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
 
-                    Console.WriteLine();
-                    Console.WriteLine("Файлы:");
-                    string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
 
-                    foreach (string s in files)   // Выведем их все
-                        Console.WriteLine(s);
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:/1/newDirectory");
+                string newPath = @"C://RECYCLE"; // тут нужно к корзине обратиться но пока не понял как
 
-                }
-                //метод, который считает количество файлов и папок в корне вашего диска и выводит итоговое количество объектов
-                try
-                {
-                    DirectoryInfo dirInfo = new DirectoryInfo(@"/" /* Или С:\\ для Windows */ );
-                    if (dirInfo.Exists)
-                    {
-                        Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
-                    }
-
-                    DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");// создание новой директории в корне вашего диска
-                    if (!newDirectory.Exists)
-                        newDirectory.Create();
-
-                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                // метод удаления каталога
-                try
-                {
-                    DirectoryInfo dirInfo = new DirectoryInfo(@"/newDirectory");
-                    dirInfo.Delete(true); // Удаление со всем содержимым
-                    Console.WriteLine("Каталог удален");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                if (dirInfo.Exists && !Directory.Exists(newPath))
+                    dirInfo.MoveTo(newPath);
             }
         }
     }
