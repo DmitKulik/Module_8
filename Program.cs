aa7d5ccefcc90f1e10257c794e365a7cb6130fc1
Dmitry <grammattika@gmail.com>
2023-05-13 23:40:50 +0300

    класс Drive для представления диска в системе

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
-using System.Numerics;
-using System.Xml.Linq;
 
 namespace Module_8{
     internal class Program{
         static void Main(){
             
+        }
+        public class Drive{ // класс Drive для представления диска в системе
+            public Drive(string _name, long _totalSpace, long _freeSpace){
+                Name = _name;
+                TotalSpace = _totalSpace;
+                FreeSpace = _freeSpace;
+            }
+            // инициализация свойства для хранения (Name TotalSpace FreeSpace) нового объекта в методе-конструкторе
+            public string Name { get; }    
+            public long TotalSpace { get; }
+            public long FreeSpace { get; }
+        }
+        public enum DriveType{
+            Usb,
+            HDD,
+            CD
         }
     }
 }
